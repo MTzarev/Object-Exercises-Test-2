a16442c0751c70e44844f26cea79b8b333be2da0
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-19 17:52:54 +0300

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,7 @@
+let name = `Plovdiv`;
+let population = 459234;
+let country = `Bulbaria`;
+let town = {name, population, country};
+town.info ={kvartal: `smirnenski`, street: `PeshterskoShose`}
+console.log(town, town.info.kvartal);
+//console.log(`${town}  ${town.info}`);  
